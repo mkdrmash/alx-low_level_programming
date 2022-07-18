65b13d1cbcd284aeb60f1b27b5eb62572eb4144d
Mohammed Kedir <mkdrmash@gmail.com>
2022-07-18 09:02:20 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int coincidence(char *a, char *b);
+void print_chessboard(char (*a)[8]);
 
 #endif
